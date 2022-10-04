b108d2f91c71fc72a7e1bd8798ae256e7abd396a
thang.pham782 <thang.pham782@hcmut.edu.vn>
2022-10-04 18:44:18 +0700

    cm

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define led_red_Pin GPIO_PIN_5
+#define led_red_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
---
+++ Core/Inc/timer.h
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ *  Created on: Oct 4, 2022
+ *      Author: WinZ
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer1_flag;
+
+void settimer1(int n);
+void timerrun();
+
+#endif /* INC_TIMER_H_ */
